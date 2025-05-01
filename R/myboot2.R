da8fc471c284962551d5d65c1eea420d23e310d8
N-A201 <NabilA@ou.edu>
2025-05-01 17:20:01 -0500

    more fixes

--- R/myboot2.R
+++ R/myboot2.R
@@ -32,16 +32,24 @@ myboot2 <- function(iter = 10000, x, fun = "mean", alpha = 0.05, cx = 1.5, ...) 
   rs.mat <- matrix(y, nrow = n, ncol = iter, byrow = TRUE)
   xstat <- apply(rs.mat, 2, fun)
   ci <- quantile(xstat, c(alpha / 2, 1 - alpha / 2))
-  para <- hist(xstat, freq = FALSE, las = 1,
-               main = paste("Histogram of Bootstrap sample statistics", "\n",
-                            "alpha=", alpha, " iter=", iter, sep = ""),
-               ...)
+  para <- hist(
+    xstat,
+    freq = FALSE,
+    las = 1,
+    main = paste(
+      "Histogram of Bootstrap sample statistics",
+      "\n",
+      "alpha=", alpha, " iter=", iter,
+      sep = ""
+    ),
+    ...
+  )
   mat <- matrix(x, nrow = length(x), ncol = 1, byrow = TRUE)
   pte <- apply(mat, 2, fun)
-  abline(v = pte, lwd = 3, col = "Black")
+  abline(v = pte, lwd = 3, col = "black")
   segments(ci[1], 0, ci[2], 0, lwd = 4)
-  text(ci[1], 0, paste("(", round(ci[1], 2), sep = ""), col = "Red", cex = cx)
-  text(ci[2], 0, paste(round(ci[2], 2), ")", sep = ""), col = "Red", cex = cx)
+  text(ci[1], 0, paste("(", round(ci[1], 2), sep = ""), col = "red", cex = cx)
+  text(ci[2], 0, paste(round(ci[2], 2), ")", sep = ""), col = "red", cex = cx)
   text(pte, max(para$density) / 2, round(pte, 2), cex = cx)
   invisible(list(ci = ci, fun = fun, x = x, xstat = xstat))
 }
